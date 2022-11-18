c5cfb6c040e6b09db5405f5fc9c02633c84f4e34
Venkatesh Sripada <s.venkatesh779@gmail.com>
2022-11-18 16:21:38 +0000

    Code with basic implementation of VP

---
+++ tdvp_preprocessing.py
@@ -0,0 +1,214 @@
+
+import os
+import csv
+import cv2
+import glob
+import numpy as np
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
+from pickle import dump
+
+from sklearn import preprocessing
+from scipy.spatial.transform import Rotation as R
+
+side_image = True
+image_height = 32
+image_width = 32
+context_length = 5
+horrizon_length = 2
+
+base_path = "/home/venky/time_delay_vp/"
+train_data_dir = base_path + "train/"
+test_data_dir = base_path + "test/"
+scaler_out_dir = base_path + 'scalar_info_universal/'
+
+train_out_dir = base_path + "train_out/"
+test_out_dir = base_path + "test_out/"
+
+class data_formatter:
+    def __init__(self):
+        self.files_train = []
+        self.files_test = []
+        self.full_data_robot_task = []
+        self.full_data_robot_joint = []
+        self.full_side_cam_data = []
+
+        self.side_image_names = []
+        self.all_reshaped = []
+        self.side_image = side_image
+        self.image_height = image_height
+        self.image_width = image_width
+        self.context_length = context_length
+        self.horrizon_length = horrizon_length
+
+    def load_file_names(self):
+        self.files_train = glob.glob(train_data_dir + '/*')
+        self.files_test = glob.glob(test_data_dir + '/*')
+
+    def scale_data(self):
+        files = self.files_train + self.files_test
+        for file in tqdm(files):
+            robot_task, robot_joint, side_camera, meta = self.load_file_data(file)
+            self.full_data_robot_task += list(robot_task)
+            self.full_data_robot_joint += list(robot_joint)
+
+        self.full_data_robot_task = np.array(self.full_data_robot_task)   # Gets the data of task space from all experiments
+        self.full_data_robot_joint = np.array(self.full_data_robot_joint)
+        
+        # Scales the data to range (0,1), basically normalizing all features (pose) across all experiments
+        self.robot_min_max_scalar = [preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(self.full_data_robot_task[:, feature].reshape(-1, 1)) for feature in range(6)]
+        self.robot_joint_min_max_scalar = [preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(self.full_data_robot_joint[:, feature].reshape(-1, 1)) for feature in range(7)]
+
+        # Standardize images
+        # Scaling the data channel-wise
+        # for i in tqdm(range(len(self.full_side_cam_data.reshape(-1,1)))):
+        #     self.full_side_cam_data_scaled = self.full_side_cam_data[i] / 255
+
+        self.save_scalars()
+
+
+    def create_map(self):
+        for stage in [train_out_dir, test_out_dir]:
+            self.path_file = []
+            index_to_save = 0
+            print(stage)
+            if stage == train_out_dir:
+                files_to_run = self.files_train
+            else:
+                files_to_run = self.files_test
+
+            for experiment_number, file in tqdm(enumerate(files_to_run)):
+                path_save = stage
+
+                robot_task, robot_joint, side_camera, meta = self.load_file_data(file)
+
+                # scale the data
+
+                for index, min_max_scalar in enumerate(self.robot_min_max_scalar):
+                    robot_task[:, index] = np.squeeze(min_max_scalar.transform(robot_task[:, index].reshape(-1, 1)))
+
+                for index, min_max_scalar in enumerate(self.robot_joint_min_max_scalar):
+                    robot_joint[:, index] = np.squeeze(min_max_scalar.transform(robot_joint[:, index].reshape(-1, 1)))
+
+                reshaped_side_image_names = []
+                if self.side_image:
+                    for time_step in range(len(side_camera)):
+                        image_name = "side_image_reshaped_" + str(experiment_number) + "_time_step_" + str(time_step) + ".npy"
+                        reshaped_side_image_names.append(image_name)
+                        np.save(path_save + image_name, side_camera[time_step])
+
+                sequence_length = self.context_length + self.horrizon_length
+
+                # len(robot_joint) is placed for this exp, create a variable that takes the minimum of robot_joint amd robot_task
+                print("\n",robot_joint.shape)
+                print(robot_task.shape)
+                print(side_camera.shape)
+                print(len(side_camera))
+                iter_length = np.min([len(robot_joint), len(robot_task), len(side_camera)])
+                for time_step in range(iter_length - sequence_length):
+                    robot_data_euler_sequence = [robot_task[time_step + t] for t in range(sequence_length)]
+                    robot_data_joint_sequence = [robot_joint[time_step + t] for t in range(sequence_length)]
+                    side_image_data_sequence     = [side_camera[time_step + t] for t in range(sequence_length)]
+                    experiment_data_sequence  = experiment_number
+                    time_step_data_sequence   = [time_step + t for t in range(sequence_length)]
+                    if self.side_image:
+                        reshaped_side_image_name_sequence = [reshaped_side_image_names[time_step + t] for t in range(sequence_length)]
+
+                    ####################################### Save the data and add to the map ###########################################
+                    np.save(path_save + 'robot_data_euler_' + str(index_to_save), robot_data_euler_sequence)
+                    np.save(path_save + 'robot_data_joint_' + str(index_to_save), robot_data_joint_sequence)
+                    np.save(path_save + 'side_image_reshaped_data_sequence_' + str(index_to_save), side_image_data_sequence)
+                    if self.side_image:
+                        np.save(path_save + 'side_image_reshaped_name_sequence_' + str(index_to_save), reshaped_side_image_name_sequence)
+                    np.save(path_save + 'experiment_number_' + str(index_to_save), experiment_data_sequence)
+                    np.save(path_save + 'time_step_data_' + str(index_to_save), time_step_data_sequence)
+                    np.save(path_save + 'trial_meta_' + str(index_to_save), np.array(meta))
+
+                    ref = []
+                    ref.append('robot_data_euler_' + str(index_to_save) + '.npy')
+                    ref.append('robot_data_joint_' + str(index_to_save) + '.npy')
+                    ref.append('side_image_reshaped_data_sequence_' + str(index_to_save) + '.npy')
+                    if self.side_image:
+                        ref.append('side_image_reshaped_name_sequence_' + str(index_to_save) + '.npy')
+                    ref.append('experiment_number_' + str(index_to_save) + '.npy')
+                    ref.append('time_step_data_' + str(index_to_save) + '.npy')
+                    ref.append('trial_meta_' + str(index_to_save) + '.npy')
+                    self.path_file.append(ref)
+                    index_to_save += 1
+
+
+            self.save_map(path_save)
+
+    def save_map(self, path, test=False):
+        if test:
+            with open(path + '/map_' + str(self.test_no) + '.csv', 'w') as csvfile:
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                writer.writerow(['robot_data_path_euler', 'robot_data_path_joint', 'side_image_data_sequence', 'side_image_name_sequence', 'experiment_number', 'time_steps', 'meta'])
+                for row in self.path_file:
+                    writer.writerow(row)
+        else:
+            with open(path + '/map.csv', 'w') as csvfile:
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                writer.writerow(['robot_data_path_euler', 'robot_data_path_joint','side_image_data_sequence', 'side_image_name_sequence', 'experiment_number', 'time_steps', 'meta'])
+                for row in self.path_file:
+                    writer.writerow(row)
+
+    def load_file_data(self, file):
+        robot_task_state = np.load(file + '/task_space_' + file[-1] + '.npy')
+        robot_joint_state = np.load(file + '/joint_states_' + file[-1] + '.npy')
+        side_camera = self.reshape_image(file)
+        # side_camera = np.load(file + '/side_camera_' + file[-1] + '.npy')
+        # convert orientation to euler:
+        robot_task_space = np.array([[state[-3], state[-2], state[-1]] + list(R.from_quat([state[-7], state[-6], state[-5], state[-4]]).as_euler('zyx', degrees=True)) for state in robot_task_state[1:]]).astype(float)
+        meta_data = np.load(file + '/meta_' + file[-1] + '.npy')
+
+        return robot_task_space, robot_joint_state, side_camera, meta_data
+
+    
+    def reshape_image(self, file):
+        all_reshaped = []
+        # for exp_no in range(1, total_exps+1):
+        try:
+            sc = np.load(file + '/side_camera_' + file[-1] + '.npy')
+        except FileNotFoundError as e:
+            print("No file exists: ", e)
+        saved = False
+        for time_step in range(sc.shape[0]):
+            img = Image.fromarray(sc[time_step], 'RGB')
+            opencv_img = np.array(img)
+            reshaped_image = cv2.resize(opencv_img, (self.image_height, self.image_width))
+
+            if not saved:
+                im_save = Image.fromarray(reshaped_image)
+                im_save.save("side_image_time_step_" + str(time_step) + ".jpeg")
+                saved = True
+            all_reshaped.append(np.array(reshaped_image))
+            # if self.side_image:
+            #     image_name = "side_image_" + str(exp_no) + "_time_step_" + str(time_step) + ".npy"
+            #     self.side_image_names.append(image_name)
+        all_reshaped = np.array(all_reshaped)
+        return all_reshaped
+
+    def save_scalars(self):
+        # save the scalars
+        dump(self.robot_min_max_scalar[0], open(scaler_out_dir + 'robot_min_max_scalar_px.pkl', 'wb'))
+        dump(self.robot_min_max_scalar[1], open(scaler_out_dir + 'robot_min_max_scalar_py.pkl', 'wb'))
+        dump(self.robot_min_max_scalar[2], open(scaler_out_dir + 'robot_min_max_scalar_pz.pkl', 'wb'))
+        dump(self.robot_min_max_scalar[3], open(scaler_out_dir + 'robot_min_max_scalar_ex.pkl', 'wb'))
+        dump(self.robot_min_max_scalar[4], open(scaler_out_dir + 'robot_min_max_scalar_ey.pkl', 'wb'))
+        dump(self.robot_min_max_scalar[5], open(scaler_out_dir + 'robot_min_max_scalar_ez.pkl', 'wb'))
+
+        dump(self.robot_joint_min_max_scalar[0], open(scaler_out_dir + 'robot_joint_min_max_scalar_j1.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[1], open(scaler_out_dir + 'robot_joint_min_max_scalar_j2.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[2], open(scaler_out_dir + 'robot_joint_min_max_scalar_j3.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[3], open(scaler_out_dir + 'robot_joint_min_max_scalar_j4.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[4], open(scaler_out_dir + 'robot_joint_min_max_scalar_j5.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[5], open(scaler_out_dir + 'robot_joint_min_max_scalar_j6.pkl', 'wb'))
+        dump(self.robot_joint_min_max_scalar[6], open(scaler_out_dir + 'robot_joint_min_max_scalar_j7.pkl', 'wb'))
+
+
+df = data_formatter()
+df.load_file_names()
+df.scale_data()
+df.create_map()
---
+++ tdvp_small_test.py
@@ -0,0 +1,308 @@
+
+import csv
+import glob
+import math
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+import torch
+import torch.nn as nn
+
+from tdvp import ACTVP
+
+class BatchGenerator:
+    def __init__(self):
+        # self.batch_size = batch_size
+        self.data_map = []
+        # self.test_directory = test_directory
+        # val = int(test_data_dir[-2]) + 34
+        # val = test_directory[-1]
+        with open(exp_test_dir + 'map.csv', 'r') as f:  # rb
+            reader = csv.reader(f)
+            for row in reader:
+                self.data_map.append(row)
+
+    def load_full_data(self):
+        dataset_test = FullDataSet(self.data_map)
+        test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
+        self.data_map = []
+        # Display image.
+        # for index, batch_features in enumerate(test_loader):
+        #     # batch_features[2] is the tensor containing side camera data of shape 32,7,32,32,3
+        #     side_camera = batch_features[2]
+        #     img = side_camera[0][0]    # First batch, first frame of sequence
+        #     img = img.cpu().detach().numpy()
+        #     im = Image.fromarray((img*255).astype(np.uint8))
+        #     im.save("Image_" + str(index) + str(batch) +".jpeg")
+        return test_loader
+
+class FullDataSet:
+    def __init__(self, data_map):
+        self.samples = data_map[1:]
+        data_map = None
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        value = self.samples[idx]
+        robot_task_data = np.load(exp_test_dir + value[0])
+        robot_joint_data = np.load(exp_test_dir + value[1])
+
+        side_camera_image_names = []
+        for image_name in np.load(exp_test_dir + value[3]):
+            side_camera_image_names.append(np.load(exp_test_dir + image_name))
+
+        experiment_number = np.load(exp_test_dir +value[4])
+        time_steps = np.load(exp_test_dir + value[5])
+        return [robot_task_data.astype(np.float32), robot_joint_data.astype(np.float32), np.array(side_camera_image_names).astype(np.float32), np.array(experiment_number).astype(np.float32), np.array(time_steps).astype(np.float32)]
+
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        super(ConvLSTMCell, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim, out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size, padding=self.padding, bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device).to(device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device).to(device))
+
+class Test:
+    def __init__(self, model_save_path):
+        self.batch_size = batch_size
+        self.current_exp = 0
+        self.index = 0
+
+        self.test_full_loader = BG.load_full_data()
+        # self.model = ACTVP()
+        self.model = torch.load(model_save_path)
+        # self.model.load_state_dict(saved_model)
+        self.criterion = nn.L1Loss()
+
+        # experiment_number_files = glob.glob(exp_test_dir + "/*") 
+        # for index, directory in enumerate(exp_test_dir):
+            
+        #     BG = BatchGenerator(batch_size = self.batch_size, test_directory=directory)
+        #     self.test_full_loader = BG.load_full_data()
+        
+        self.test_model()
+        self.calculate_test_performance()
+
+    def test_model(self):
+        batch_losses = []
+        self.performance_data = []
+
+        for index, batch_features in enumerate(self.test_full_loader):
+            
+            # self.test_image(batch_features)
+            groundtruth_scene, predictions_scene = self.format_and_run_batch(batch_features, index)
+            self.calculate_test_performance() 
+            print("Here")
+
+            # Quantitative analysis
+            # batch_losses.append(self.calculate_scores(predictions_scene, groundtruth_scene[context_frames:]))
+
+            # # Qualitative analysis
+            print(type(int(batch_features[4][0][5].item())))
+            # Looking at time steps 5 to 14 across all experiments and saving them
+            if (int(batch_features[4][0][5].item() )) == int(5): # and batch_features[5][0] == 25
+                print("In here")
+                # self.save_images(batch_features, groundtruth_scene, predictions_scene)
+            
+
+            # if batch_features[4] == 5 and batch_features[5][0] == 25:   
+            #     self.save_images(predictions_scene, groundtruth_scene[context_frames:], index)
+    
+
+    def test_image(self, batch_features, index = None, modify_img_shape=False):
+        # side_camera = batch_features[2].to(device)
+        side_camera_cut = batch_features
+
+        # Perform this in post processing
+        img = side_camera_cut[0][0]    # 0th frame in sequence, 0th batch
+        img = img.cpu().detach().numpy()
+        if modify_img_shape:
+            img = np.transpose(img, (1,2,0))  # Image to be 32, 32, 3
+        img = np.rot90(np.fliplr(img))
+        im = Image.fromarray((img*255).astype(np.uint8)).convert("RGB")
+        # im.save("Image_gt.jpeg")
+        im.save("Image_gt" + str(index) + ".jpeg")
+
+    def pad_data(self, batch_features):
+        dim_task_space = batch_features[0].shape
+        dim_joint_space = batch_features[1].shape
+        dim_side_cam = batch_features[2].shape
+        dim_exp = batch_features[3].shape
+        dim_time = batch_features[4].shape
+
+        zeros_task_data = torch.zeros(self.batch_size - len(batch_features[0]), dim_task_space[1], dim_task_space[2])
+        zeros_joint_data = torch.zeros(self.batch_size - len(batch_features[1]), dim_joint_space[1], dim_joint_space[2])
+        zeros_side_cam = torch.zeros(self.batch_size - len(batch_features[2]), dim_side_cam[1], dim_side_cam[2], dim_side_cam[3], dim_side_cam[4])
+        zeros_exp = torch.zeros(self.batch_size - len(batch_features[3]))
+        zeros_time = torch.zeros(self.batch_size - len(batch_features[4]), dim_time[1])
+
+        # Pad with zeros to make length same as batch size
+        robot_task = torch.cat((batch_features[0], zeros_task_data ))
+        robot_joint = torch.cat((batch_features[1], zeros_joint_data ))
+        side_camera = torch.cat((batch_features[2], zeros_side_cam))
+        exp = torch.cat((batch_features[3], zeros_exp))
+        time_data = torch.cat((batch_features[4], zeros_time))
+
+        return robot_task, robot_joint, side_camera, exp, time_data
+
+
+    def format_and_run_batch(self, batch_features, index):
+        
+        mae, kld, mae_side_cam, predictions = 100, 100, 100, 100
+        self.prediction_data = []
+        current_batch = len(batch_features[0])
+        if len(batch_features[2]) == self.batch_size:   # side_camera_data
+            side_camera = batch_features[2].permute(1, 0, 4, 3, 2).to(device)
+            robot_task = batch_features[0].squeeze(-1).permute(1, 0, 2).to(device)
+            i = 1
+
+            # self.test_image(batch_features)
+        else:
+            
+            robot_task, robot_joint, side_camera, experiment_number, time_steps = self.pad_data(batch_features)
+            
+            side_camera = batch_features[2].permute(1, 0, 4, 3, 2).to(device)
+            robot_task = batch_features[0].squeeze(-1).permute(1, 0, 2).to(device)
+            i = 0
+
+        side_camera_predictions = self.model.forward(side_camera=side_camera,
+                                                        robot_task_data=robot_task)  # Step 3. Run our forward pass.
+        
+        if i == 0:
+            side_camera_cut = side_camera[:, 0:current_batch, :, :, :]
+            side_camera_predictions_cut = side_camera_predictions[:, 0:current_batch, :, :, :]
+        if i == 1:
+            side_camera_cut = side_camera[:, :, :, :, :]
+            side_camera_predictions_cut = side_camera_predictions[:, :, :, :, :]
+
+        # Saving the image
+        self.test_image(side_camera_cut, index=index, modify_img_shape=True)
+
+        self.prediction_data.append(
+            [side_camera_cut[context_frames:].cpu().detach(), side_camera_predictions_cut.cpu().detach()])
+
+        print ("currently testing trial number: ", str (self.current_exp))
+        # self.calc_train_trial_performance()
+            
+        self.calc_trial_preformance()
+        # self.save_predictions(self.current_exp)
+        # self.create_test_plots(self.current_exp)
+        # self.create_difference_gifs(self.current_exp)
+        # self.prediction_data = []
+
+        # Takes the value of experiment_number which is a tensor with all 0s or 1s etc.. self.curent_exp should be same as the experiment_number for current_batch to imcrement
+        self.current_exp += 1   
+    
+        # returning grouth truth and predictions
+        return self.prediction_data[0][0], self.prediction_data[0][1]
+
+    def calc_trial_preformance(self):
+        mae_loss = 0.0
+        index = 0
+        index_ssim = 0
+        with torch.no_grad ():
+            for batch_set in self.prediction_data:
+                index += 1
+
+                mae_loss_check = self.criterion (batch_set[0], batch_set[1]).item ()
+                if math.isnan (mae_loss_check):
+                    index -= 1
+                else:
+                    ## MAE:
+                    mae_loss += mae_loss_check
+
+        self.performance_data.append (mae_loss / index)
+
+    def calculate_test_performance(self):
+        '''
+        - Calculates PSNR, SSIM, MAE for ts1, 5, 10 and x,y,z forces
+        - Save Plots for qualitative analysis
+        - Slip classification test
+        '''
+        performance_data_full = []
+        performance_data_full.append (["test loss MAE(L1): ", (
+                    sum (self.performance_data) / len (self.performance_data))])
+
+        [print (i) for i in performance_data_full]
+        np.save (data_save_path + 'model_performance_loss_data', np.asarray (performance_data_full))
+
+
+    def calculate_scores(self, prediction_scene, groundtruth_scene, prediction_tactile=None, groundtruth_tactile=None):
+        scene_losses_full, scene_losses_last = [],[]
+        for criterion in [nn.L1Loss(), nn.MSELoss()]:  #, SSIM(window_size=self.image_width)]:
+            scene_batch_loss_full = []
+            for i in range(prediction_scene.shape[0]):
+                scene_batch_loss_full.append(criterion(prediction_scene[i], groundtruth_scene[i]).cpu().detach().data)
+
+            scene_losses_full.append(sum(scene_batch_loss_full) / len(scene_batch_loss_full))
+            scene_losses_last.append(criterion(prediction_scene[-1], groundtruth_scene[-1]).cpu().detach().data)  # t+5
+
+        return [scene_losses_full, scene_losses_last]
+
+
+    def save_images(self, batch_features, side_camera, side_camera_predictions):
+
+        # for index, batch in enumerate(batch_features):
+        for index in range(len(batch_features)):
+
+            experiment = str(int(batch_features[3][0].item()))
+            time_step = str(int(batch_features[4][0][0].item()))
+
+            gt = side_camera[index][index].cpu().detach().numpy()
+            gt = np.transpose(gt, (1,2,0))
+            im = Image.fromarray((gt * 255).astype(np.uint8)).convert("RGB")
+            im.save("imageGT2_ " + experiment  + " time_step_" +  time_step + ".jpeg")
+
+            pred = side_camera_predictions[index][index].cpu().detach().numpy()
+            pred = np.transpose(pred, (1,2,0))
+            im = Image.fromarray((pred * 255).astype(np.uint8)).convert("RGB")
+            im.save("imagePRED2_ " + experiment  + " time_step_" +  time_step + ".jpeg")
+
+            # for image_gt, image_pred in zip(side_camera[index][context_frames:], side_camera_predictions[index]):
+            #     print("Saving Image")
+            #     im = Image.fromarray(image_gt)
+            #     im.save("imageGT_ " + experiment  + " time_step_" +  time_step + ".jpeg")
+
+            #     im = Image.fromarray(image_pred)
+            #     im.save("imagePRED_ " + experiment  + " time_step_" +  time_step + ".jpeg")
+            
+                # save numpy:
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")#  use gpu if available
+    context_frames = 5
+    batch_size = 32
+
+    data_save_path  = "/home/venky/time_delay_vp/models/THDloss_model_18_11_2022_11_17/scaled_test/"
+    model_save_path = "/home/venky/time_delay_vp/models/THDloss_model_18_11_2022_11_17/ACTVP_THD"
+    # test_data_dir   = "/home/venky/time_delay_vp/test_out/test_trial_0/"
+    exp_test_dir   = "/home/venky/time_delay_vp/test_out/"
+
+    BG = BatchGenerator()
+    t = Test(model_save_path)
+    # t.test_model()
